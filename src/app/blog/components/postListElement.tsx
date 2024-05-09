2143b9c73d2e20e9121d22c63edba09dab04401e
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-05-09 16:17:04 +0200

    Fix post list element, take care of description, responsive design related features

--- src/app/blog/components/postListElement.tsx
+++ src/app/blog/components/postListElement.tsx
@@ -13,12 +13,18 @@ export default function PostListElement({ post }: { post: Post }) {
        rounded-xl p-5
        hover:border-california-100 hover:shadow-lg hover:shadow-california-100/20"
       >
-        <div className="w-full flex flex-col gap-2 text-sm sm:text-xl items-start px-2 sm:px-5">
-          <span className="font-semibold">{post.metadata.title}</span>
-          <span className="text-xs sm:text-sm font-thin">
-            {post.metadata.publishDate}
-          </span>
-          {/*  TODO: description and tags*/}
+        <div className="w-full flex flex-col gap-2 items-start px-2 sm:px-5">
+          <div className="w-full flex flex-col space-y-2 justify-between items-start sm:flex-row-reverse sm:items-center">
+            <p className="text-xs sm:text-sm font-thin">
+              {post.metadata.publishDate}
+            </p>
+            <p className="text-sm sm:text-xl font-semibold">
+              {post.metadata.title}
+            </p>
+          </div>
+          <p className="font-light max-sm:text-xs line-clamp-2">
+            {post.metadata.description}
+          </p>
         </div>
       </div>
     </Link>
