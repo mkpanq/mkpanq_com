acf8441bd3bba90b6317f9942d6dc6921d79da9f
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-05-14 19:13:31 +0200

    Add new blog post

--- src/app/blog/_lib/helpers/contentTransformer.ts
+++ src/app/blog/_lib/helpers/contentTransformer.ts
@@ -4,6 +4,7 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeStringify from "rehype-stringify";
+import rehypeClassNames from "rehype-class-names";
 
 export default async function convertMarkdownToHTML(
   postContent: Post["content"],
@@ -12,6 +13,9 @@ export default async function convertMarkdownToHTML(
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeExternalLinks, { target: "_blank" })
+    .use(rehypeClassNames, {
+      img: "mx-auto",
+    })
     .use(rehypeStringify)
     .process(postContent);
   return { __html: processedContent.toString() };
