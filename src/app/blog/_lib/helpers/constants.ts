f026ea6a532594fd41127048db9b3c0dcfc9d00c
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-11-02 18:24:31 +0100

    New site

--- src/app/blog/_lib/helpers/constants.ts
+++
@@ -1,4 +0,0 @@
-import path from "node:path";
-
-const POSTS_FOLDER_NAME = "posts";
-export const POSTS_FOLDER_PATH = path.join(process.cwd(), POSTS_FOLDER_NAME);
--- src/lib/envSchema.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-const envSchema = z.object({
-  NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: z.string(),
-});
-
-export const envVars = envSchema.parse({
-  NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
-});
