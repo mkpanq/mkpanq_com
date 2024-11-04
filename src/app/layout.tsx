b40bf4834d57921ef8215dd50144b356fedeef6f
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-11-04 17:00:21 +0100

    Better SEO, add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,83 @@ const urbanist = Urbanist({
 });
 
 export const metadata: Metadata = {
-  title: "mkpanq.com",
-  description: "Marek Pankowski - Homepage",
+  metadataBase: new URL("https://mkpanq.com"),
+  applicationName: "mkpanq.com | Marek Pankowski",
+  creator: "Marek Pankowski",
+  publisher: "Marek Pankowski",
+  openGraph: {
+    siteName: "mkpanq.com | Marek Pankowski",
+    description:
+      "Marek Pankowski personal website of - Full Stack Product Engineer from Poland",
+    type: "website",
+    locale: "en_US",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+      "max-video-preview": -1,
+    },
+  },
+  appleWebApp: {
+    title: "mkpanq.com | Marek Pankowski",
+    statusBarStyle: "black-translucent",
+    capable: true,
+  },
+  // TODO: For later
+  // verification: {
+  //   google: "YOUR_DATA",
+  //   yandex: ["YOUR_DATA"],
+  //   other: {
+  //     "msvalidate.01": ["YOUR_DATA"],
+  //     "facebook-domain-verification": ["YOUR_DATA"],
+  //   },
+  // },
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        type: "image/x-icon",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    shortcut: [
+      {
+        url: "/favicon.ico",
+        type: "image/x-icon",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
